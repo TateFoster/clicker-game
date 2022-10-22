3c90927ca954645d9faa01c0c839817d74d80afc
Dragon Lore <tate.j.foster@gmail.com>
2022-10-22 14:28:14 -0600

    started puting in scary quotes for when timer runs out

---
+++ client/src/components/ClickArea/Quotes/Quotes.js
@@ -0,0 +1,12 @@
+import "./Quotes.css";
+
+const quotes = [
+	"Did you think that you could escape?",
+	"Not even death shall grant you rest.",
+	"Why not try again? Maybe you will be luckier.",
+	"I always see you",
+	"There is no escape.",
+	"You deserve this.",
+	"You know what you did.",
+	"We can smell your fear.",
+];
