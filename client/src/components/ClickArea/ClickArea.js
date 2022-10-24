0cbcb037439241854194890066e01f951d37b34b
Dragon Lore <tate.j.foster@gmail.com>
2022-10-24 15:51:09 -0600

    setting up counter/ maxCount useStates

--- client/src/components/ClickArea/ClickArea.js
+++ client/src/components/ClickArea/ClickArea.js
@@ -2,12 +2,20 @@ import "./ClickArea.css";
 import Trees from "./Backgrounds/trees.jpg";
 import Quotes from "./Quotes/Quotes.js";
 import Counter from "./Counter/Counter.js";
+import { useState } from "react";
 
 export default function ClickArea() {
+	const [counterMax, setCounterMax] = useState(10);
+	const [counter, setCounter] = useState(counterMax);
+
+	const addTime = () => {
+		setCounter(counter + counterMax / 1000);
+	};
+
 	return (
 		<div className="clickArea">
 			<Counter />
-			<img className="background" src={Trees}></img>
+			<img className="background" src={Trees} onClick={addTime}></img>
 			<Quotes />
 		</div>
 	);
